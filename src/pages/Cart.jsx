b4ead82acbb3d8fb50b2c885d560480d75f19e46
iveeakter78~ <f82058878@gmail.com>
2025-09-21 10:30:04 -0700

    demo

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,20 +7,13 @@ import productimg from "../assets/glass.png"
 import { useSelector } from 'react-redux'
 
 const Cart = () => {
-    const data = useSelector(state =>state.cartDetails.cartItems)
-    // console.log(data);
-    const singleItems = data.map((item)=> item.title)
-     console.log(singleItems);
+    const data = useSelector(state =>state.cartDetails.cartItems) 
+    console.log(data, "Data");
      
   return (
     <div>
         <Container>
             <h2 className='mt-[124px] font-dm font-bold text-[49px] capitalize'>Cart</h2>
-            {
-                data.map((item)=>
-                    <h2>item.title</h2>
-                )
-            }
             <span className='pt-[15px] flex items-center gap-x-1'>
               <Link to={"/"}><p className='text-[#6D6D60] font-dm  text-[12px] capitalize'>Home</p></Link> &gt; 
               <Link to={"/cart"}><p className='text-[#6D6D60] font-dm  text-[12px] capitalize'>Cart</p></Link>
@@ -39,34 +32,44 @@ const Cart = () => {
                     <p className='font-dm font-bold text-[16px] text-primary capitalize'>Total</p>
                 </div>
             </div>
-             <div className='w-full flex justify-between items-center py-3 px-2 border-1 border-[#F0F0F0]'>
-                <div className='w-[25%]'>
+            {
+                data.length > 0 ?
+                 data?.map((item)=>(
+                    <div className='w-full flex justify-between items-center py-3 px-2 border-1 border-[#F0F0F0]'>
+                        <div className='w-[25%]'>
                     <div className='flex gap-x-4 items-center'>
                         <div>
                             <RxCross2 />
                         </div>
                         <div className='w-[28%]'>
-                            <img className='w-full' src={productimg} alt="" />
+                            <img className='w-full' src={item.thumbnail} alt="" />
                         </div>
                         <div>
-                            <p className='font-dm font-bold text-[16px] capitalize text-primary'>Product name</p>
+                            <p className='font-dm font-bold text-[16px] capitalize text-primary'>{item.title}</p>
                         </div>
                     </div>
                 </div>
                    <div className='w-[25%]'>
-                    <p className='font-dm font-bold text-[20px] text-primary '>$44.00</p>
+                    <p className='font-dm font-bold text-[20px] text-primary '>${item.price}</p>
                 </div>
                    <div className='w-[25%]'>
         <div className='flex items-center gap-x-4 border-2 border-[#F0F0F0] w-fit px-8 py-2'>
           <p>-</p>
-          <p>1</p>
+          <p>{item.cartQun}</p>
           <p>+</p>
         </div>
                 </div>
                    <div className='w-[25%]'>
-                    <p className='font-dm font-bold text-[20px] text-primary'>$44.00</p>
+                    <p className='font-dm font-bold text-[20px] text-primary'>${
+                     item.price * item.cartQun
+                    
+                    }</p>
                 </div>
-            </div>
+                    </div>
+                    ))
+                 :
+                "no items"
+            }
               <div className='w-full flex justify-between items-center py-3 px-2 border-1 border-t-0 border-[#F0F0F0]'>
                 <div className='w-[25%]'>
            <select className='px-11 py-2 border-2 border-[#F0F0F0]'>
--- src/slice/cartSlice.js
+++ src/slice/cartSlice.js
@@ -7,14 +7,26 @@ const initialState = {
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-  cartItems: [],
+  // cartItems: [],
+   cartItems: localStorage.getItem("cartDetails") ? JSON.parse(localStorage.getItem
+    ("cartDetails")): []
   },
   reducers: {
     cartTotal: (state, action) => {
-      console.log(state.cartItems,"state");
-      console.log(action.payload,"action");
-      state.cartItems.push(action.payload);
+      const findIndex = state.cartItems.findIndex((item)=> item.id
+    == action.payload.id)
+   
+    console.log(findIndex);
+    
+     if(findIndex >= 0){
+        state.cartItems[findIndex].cartQun += 1
+     }else{
+        state.cartItems.push({...action.payload, cartQun: 1})
+     }
+
+      localStorage.setItem("cartDetails", JSON.stringify(action.payload))
     },
+    
   },
 })
 
