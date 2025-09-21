37edd00c0a87cb0b76a326f5d30b67e21398a142
iveeakter78~ <f82058878@gmail.com>
2025-09-21 00:29:05 -0700

    demo

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { cartSlice } from './slice/CartSlice'
+import  cartSlice  from './slice/CartSlice'
 
 export const store = configureStore({
   reducer: {
