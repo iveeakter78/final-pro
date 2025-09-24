487b99a5a73894680cdc8bd7d9acb10d57cbf81f
iveeakter78~ <f82058878@gmail.com>
2025-09-23 21:44:24 -0700

    demo

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { store } from './store.js'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider store = {store}>
     <ContextApi>
       <App />
     </ContextApi>
