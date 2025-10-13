601ccfa7525271811627bfed8bec5229dbb9a248
iveeakter78~ <f82058878@gmail.com>
2025-10-12 21:35:12 -0700

    demo

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { ContextApi } from './components/ContextApi.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store.js'
-import firebase from 'firebase/compat/app'
+
 createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
     <ContextApi>
